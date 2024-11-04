a8ba5347d2bfb9bef0dc036b9496637a3c83ba5b
Ilya <ilya.yunkins@gmail.com>
2024-11-04 21:57:30 +0200

    Refactor panel visibility and add menu items

--- main.cpp
+++ main.cpp
@@ -14,88 +14,102 @@ private:
     void OnButton2Click(wxCommandEvent& event);
     void OnButton3Click(wxCommandEvent& event);
 
-    wxPanel* panel1;
-    wxPanel* panel2;
-    wxPanel* panel3;
+    wxPanel* training;
+    wxPanel* nutrition;
+    wxPanel* profile;
+    wxPanel* mainPage;
     wxBoxSizer* mainSizer;
-    wxStaticText* textOnPanel2;
+    wxBoxSizer* buttonSizer;
+    wxStaticText* textOnNutrition;
+
+    wxButton* button1;
+    wxButton* button2;
+    wxButton* button3;
 };
 
-// Реализация метода инициализации приложения
 bool MyApp::OnInit() {
     MyFrame* frame = new MyFrame("Приложение на wxWidgets");
     frame->Show(true);
     return true;
 }
 
-// Реализация главного окна
+
 MyFrame::MyFrame(const wxString& title)
        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600)) {
     
-    wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
-
-    wxButton* button1 = new wxButton(this, wxID_ANY, "Button 1");
-    wxButton* button2 = new wxButton(this, wxID_ANY, "Button 2");
-    wxButton* button3 = new wxButton(this, wxID_ANY, "Button 3");
+    buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+    //Main menu buttons
+    button1 = new wxButton(this, wxID_ANY, "Training");
+    button2 = new wxButton(this, wxID_ANY, "Journal");
+    button3 = new wxButton(this, wxID_ANY, "Profile");
 
     buttonSizer->Add(button1, 1, wxALL, 5);
     buttonSizer->Add(button2, 1, wxALL, 5);
     buttonSizer->Add(button3, 1, wxALL, 5);
 
-    // Привязка событий к кнопкам
+    // Binding events to buttons
     button1->Bind(wxEVT_BUTTON, &MyFrame::OnButton1Click, this);
     button2->Bind(wxEVT_BUTTON, &MyFrame::OnButton2Click, this);
     button3->Bind(wxEVT_BUTTON, &MyFrame::OnButton3Click, this);
 
-    // Создаём панели для каждого экрана
-    panel1 = new wxPanel(this, wxID_ANY);
-    panel2 = new wxPanel(this, wxID_ANY);
-    panel3 = new wxPanel(this, wxID_ANY);
+    // Creating panels for each menu item
+    training = new wxPanel(this, wxID_ANY);
+    nutrition = new wxPanel(this, wxID_ANY);
+    profile = new wxPanel(this, wxID_ANY);
+    mainPage = new wxPanel(this, wxID_ANY);
 
     // Настраиваем контент для каждой панели
-    panel1->SetBackgroundColour(*wxBLUE);
-    panel2->SetBackgroundColour(*wxGREEN);
-    panel3->SetBackgroundColour(*wxRED);
+    training->SetBackgroundColour(*wxBLUE);
+    nutrition->SetBackgroundColour(*wxGREEN);
+    profile->SetBackgroundColour(*wxRED);
+    mainPage->SetBackgroundColour(*wxWHITE);
 
-    // Создаём текстовый элемент на panel2 и выравниваем его по центру
-    wxBoxSizer* panel2Sizer = new wxBoxSizer(wxVERTICAL);
-    textOnPanel2 = new wxStaticText(panel2, wxID_ANY, "Текст на панели 2");
-    panel2Sizer->Add(textOnPanel2, 1, wxALIGN_CENTER | wxALL, 10);
-    panel2->SetSizer(panel2Sizer);
+    // Adding elements to the nutrition panel
+    wxBoxSizer* nutritionSizer = new wxBoxSizer(wxVERTICAL);
+    textOnNutrition = new wxStaticText(nutrition, wxID_ANY, "Текст на панели 2");
+    nutritionSizer->Add(textOnNutrition, 1, wxALIGN_CENTER | wxALL, 10);
+    nutrition->SetSizer(nutritionSizer);
 
-    // Скрываем panel2 и panel3 по умолчанию
-    panel2->Hide();
-    panel3->Hide();
+    // hide all panels except the first one
+    mainPage->Show();
+    training->Hide();
+    nutrition->Hide();
+    profile->Hide();
 
-    // Добавляем кнопки и панели в основной sizer
+    // add buttons and panels to the main sizer
     mainSizer = new wxBoxSizer(wxVERTICAL);
+
     mainSizer->Add(buttonSizer, 0, wxEXPAND); 
-    mainSizer->Add(panel1, 1, wxEXPAND);      // Начальная видимая панель
-    mainSizer->Add(panel2, 1, wxEXPAND);      // Панель 2 скрыта по умолчанию
-    mainSizer->Add(panel3, 1, wxEXPAND);      // Панель 3 скрыта по умолчанию
+    mainSizer->Add(mainPage, 1, wxEXPAND);
+    mainSizer->Add(training, 1, wxEXPAND);      
+    mainSizer->Add(nutrition, 1, wxEXPAND);     
+    mainSizer->Add(profile, 1, wxEXPAND);      
 
     SetSizer(mainSizer);
 }
 
 // Обработчики событий для кнопок
 void MyFrame::OnButton1Click(wxCommandEvent& event) {
-    panel1->Show();
-    panel2->Hide();
-    panel3->Hide();
+    mainPage->Hide();
+    training->Show();
+    nutrition->Hide();
+    profile->Hide();
     mainSizer->Layout();
 }
 
 void MyFrame::OnButton2Click(wxCommandEvent& event) {
-    panel1->Hide();
-    panel2->Show();
-    panel3->Hide();
+    mainPage->Hide();
+    training->Hide();
+    nutrition->Show();
+    profile->Hide();
     mainSizer->Layout();
 }
 
 void MyFrame::OnButton3Click(wxCommandEvent& event) {
-    panel1->Hide();
-    panel2->Hide();
-    panel3->Show();
+    mainPage->Hide();
+    training->Hide();
+    nutrition->Hide();
+    profile->Show();
     mainSizer->Layout();
 }
 
